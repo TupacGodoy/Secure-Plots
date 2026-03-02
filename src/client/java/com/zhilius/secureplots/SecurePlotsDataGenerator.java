5b30c7291d1171e3d1dcb15e0c92bdc8cb678e5b
TupacGodoy <stgodoy@fi.uba.ar>
2026-03-02 04:11:07 -0300

    basic structure

---
+++ src/client/java/com/zhilius/secureplots/SecurePlotsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.zhilius.secureplots;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SecurePlotsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
